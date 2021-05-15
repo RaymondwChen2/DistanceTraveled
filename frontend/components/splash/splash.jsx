447c0cc617a08a5439fa6c4ee4374e4a213ce97a
Raymond Chen <raymondwchen2@gmail.com>
2021-05-14 18:09:12 -0700

    change splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Splash = () => {
   return (
     <div className='splash-container'>
-        <div className='splash'></div>
+      <div>
+        <div className='splash'>
+          <h2>
+          Your Journey Starts Here
+          </h2>
+          <p>the app that will change the way you workout</p>
+          </div>
+
+      </div>
         <div className='splashDiag' ></div>
         <div className='splash2'></div>
     </div>
