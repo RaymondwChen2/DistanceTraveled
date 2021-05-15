af9ac55d158d5a50863aa5965e2d4e30f2a51ba2
Raymond Chen <raymondwchen2@gmail.com>
2021-05-14 17:29:46 -0700

    Style splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Splash = () => {
   return (
     <div className='splash-container'>
         <div className='splash'></div>
+        <div className='splashDiag'></div>
         <div className='splash2'></div>
     </div>
     
