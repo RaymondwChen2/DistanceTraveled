46a2b4c43a06dc604b0d24823eddc45db3727ebf
Raymond Chen <raymondwchen2@gmail.com>
2021-05-17 22:36:13 -0700

    add sign up/ login button to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Splash = () => {
   return (
     <div className='splash-container'>
       <div>
         <div className='splash'>
-          <h2>
-          Your Journey Starts Here
-          </h2>
-          <p>the app that will change the way you workout</p>
+          <h2>Your Journey Starts Here</h2>
+          <p>The app that will change the way you workout</p>
+          <div><Link to="/signup" className='splash-signup'>SIGN UP</Link></div>
+          <div className='splash-login'>Already a member? <Link to="/login" className='login-button'>LOGIN</Link></div>
           </div>
 
       </div>
