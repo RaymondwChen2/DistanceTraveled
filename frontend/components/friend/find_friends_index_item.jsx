9742c8a349aa45e6c814abeca6cd164e648700c2
raymond <raymondwchen2@gmail.com>
2021-08-24 22:31:34 -0700

    Create find friend index item

---
+++ frontend/components/friend/find_friends_index_item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class FindFriendIndexItem extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        let { user } = this.props;
+        return(
+            <div>
+                <Link></Link>
+            </div>
+        )
+    }
+}
